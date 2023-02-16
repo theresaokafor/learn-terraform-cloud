7d462940944642b20649e48d752080fb2fbf90ba
Tessy Okafor <theresa.okafor21@gmail.com>
2023-02-16 11:21:51 -0500

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "tess-oka4"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
